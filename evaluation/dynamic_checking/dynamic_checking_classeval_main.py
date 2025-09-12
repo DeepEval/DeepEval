04a56c82a346256b8d820d035e361094ba62b989
maxy-635 <xiangyue_ma@163.com>
2025-09-12 13:25:22 +0800

    result of RQ1

---
+++ evaluation/dynamic_checking/dynamic_checking_classeval_main.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import json
+import pandas as pd
+import subprocess
+from tabulate import tabulate
+from utils.df2excel import DataFrame2Excel
+from utils.getallfiles import GetAllFiles
+from utils.count_code_lines import count_code_lines
+from my_compile_utils.get_task_name import get_task_name
+
+class Validation:
+    """
+    validate the code by compiling the code
+    """
+    def __init__(self):
+        pass
+
+    def compile_code(self, pyfile):
+        result=subprocess.run(["python", pyfile], capture_output=True, text=True)
+        
+        if result.returncode==0:
+            compile_status = "compile success"
+            compile_error = None
+        else:
+            compile_status = "compile failed"
+            compile_error = result.stderr or result.stdout
+
+        with open(pyfile,"r",encoding='utf-8') as file:
+            python_code=file.read()
+
+        verification = {"pyfile_path": str(pyfile),
+                        "compile_status": str(compile_status),
+                        "compile_error": str(compile_error)}
+        
+        return verification
+
+    def batch_compile_codes(self, code_path):
+        pyfile_paths = GetAllFiles(code_path, ".py").get_all_files_in_directory()
+        verifications = []
+
+        for pyfile_path in pyfile_paths:
+            try:
+                verification = self.compile_code(pyfile_path)
+                verifications.append(verification)
+                print(f'-----compile success:{verification} -----')
+            except Exception as e:
+                print(f'-----compile failed: {e} -----')
+
+        return verifications
+
+    def main(self, promptings, llms, experiment_batch):
+        global df_data, pd_rows
+        for llm in llms:
+            dfs_last_row ,dfs_last_col = [], []
+            for prompting in promptings:
+                
+                code_path = os.path.join(common_root_path, f'response/ClassEval/{llm}/{prompting}/{experiment_batch}')
+                verifications = self.batch_compile_codes(code_path)
+
+                save_json_path = os.path.join(common_root_path,f"evaluation/dynamic_checking/baselines/ClassEval/{llm}/{experiment_batch}/{prompting}.json")
+
+                if not os.path.exists(os.path.dirname(save_json_path)):
+                    os.makedirs(os.path.dirname(save_json_path))
+                with open(save_json_path, 'w') as f: 
+                    json.dump(verifications, f, indent=2)
+
+                tasks_names = get_task_name(code_path) 
+                count_dict_list = []
+
+                for task_name in tasks_names: 
+                    count_dict = {"Benchmark": task_name, "success": 0, "failed": 0, "main_entry": 0, "valid_pyfiles": 0, "rate(%)": 0}
+                    valid_pyfiles = 0
+                    for pyfile in os.listdir(os.path.join(code_path, task_name)):
+                        if pyfile.endswith('.py'):
+                            if count_code_lines(os.path.join(code_path, task_name, pyfile)) != 0:
+                                valid_pyfiles += 1
+
+                    for item in verifications:
+                        if task_name + '/' in item['pyfile_path']: 
+                            if item['compile_status'] == 'compile success':
+                                count_dict["success"] += 1
+                            else:
+                                count_dict["failed"] += 1
+                            if item["main_entry"] == 'exist':
+                                count_dict["main_entry"] += 1
+
+                    count_dict["valid_pyfiles"] = valid_pyfiles
+                    count_dict["rate(%)"] = '-'
+
+                    count_dict_list.append(count_dict)
+
+                df_data = pd.DataFrame(count_dict_list)
+                Total = df_data.sum(axis=0)
+                Total['Benchmark'] = 'Total'
+                df_data = pd.concat([df_data, Total.to_frame().T], ignore_index=True)
+                total_failed = df_data.loc[df_data['Benchmark'] == 'Total', 'failed'].values[0]
+                total_valid_pyfiles = df_data.loc[df_data['Benchmark'] == 'Total', 'valid_pyfiles'].values[0]
+                total_rate = 100 * (total_valid_pyfiles - total_failed)/ total_valid_pyfiles
+
+                df_data.loc[df_data['Benchmark'] == 'Total', 'rate(%)'] = total_rate
+
+                print(tabulate(df_data, headers='keys', tablefmt='grid'))
+                save_result_excel_path = os.path.join(common_root_path,
+                            f"evaluation/dynamic_checking/baselines/ClassEval/{llm}/{llm}_{experiment_batch}.xlsx")
+                DataFrame2Excel(df_data, save_result_excel_path).df2excel(sheet_name=prompting)
+
+            print(f"Dynamic Checking for {llm} {experiment_batch} is finished.")
+
+
+if __name__ == '__main__':
+    common_root_path = "/your_local_path/DeepEval"
+    promptings = ['classeval']
+
+    llms = ["gpt_4o",
+            "gpt_4o_mini", 
+            "codegemma_7b_it", 
+            "codellama_7b_instruct_hf",
+            "deepseek_coder_v2_lite_instruct",
+            "deepseek_v2_lite_chat",
+            "gemma_2_9b_it",
+            "meta_llama_3_1_8b_instruct"]
+
+    compile_validation = Validation()
+    compile_validation.main(promptings, llms)
